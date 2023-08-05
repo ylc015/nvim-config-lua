6a62478fc8bf2c0ac7bba5aef4fe8d99be5c6d1e
Peter Chan <peter.chan@owl.co>
2023-08-05 13:32:59 -0400

    add custom zprint function

---
+++ lua/custom/functions/init.lua
@@ -0,0 +1,34 @@
+function InstallZprintFiler()
+	local home_dir=os.getenv("HOME")
+	local zprint_version = '1.2.7'
+	local zprint_path = home_dir .. "/zprintm-" .. zprint_version
+	local f=io.open(zprint_path)
+
+	if vim.fn.executable('wget') ~= 1 then
+		error('wget is not installed. please install first')
+		return false
+	end
+
+	if f~=nil then
+		io.close(f)
+	        print("zprint exists " .. zprint_path)
+	else
+	        print("zprint does not exists at " .. zprint_path .. " installing")
+		local command = "cd " .. home_dir .. " && curl -LJO https://github.com/kkinnear/zprint/releases/download/" .. zprint_version .. "/zprintm-".. zprint_version .. " && chmod +x " .. zprint_path
+		-- local command = "cd " .. home_dir .. " && echo 'this is test' > nvim.test" 
+
+		print("Command is " .. command)
+		local status, err = pcall(os.execute, command)
+		if status then
+			print("command ran successfuly")
+		else
+			print("failed to run command " .. err)
+		end
+	end
+	vim.keymap.set('v', '<leader>cf',  ":'<,'>!" .. zprint_path .. "<cr>")
+
+end
+
+
+vim.api.nvim_create_user_command('InstallZprint', InstallZprintFiler, {})
+
