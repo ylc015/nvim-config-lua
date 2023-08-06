f777a032d83d018f2e83aebd38b1a99f07dbd6cb
Peter Chan <peter.chan@owl.co>
2023-08-06 12:30:00 -0400

    comment out print statement in lua config

--- lua/custom/functions/init.lua
+++ lua/custom/functions/init.lua
@@ -8,7 +8,7 @@ local zprint_path = home_dir .. "/" .. zprint_file
 
 function InstallZprintFiler()
 	local f=io.open(zprint_path)
-	print("setting key with " .. zprint_path)
+	-- debug("setting key with " .. zprint_path)
 	vim.keymap.set('v', '<leader>cf',  ":'<,'>!" .. zprint_path .. "<cr>")
 	vim.keymap.set('n', '<leader>cx',  ":'<,'>!somethingdump<cr>")
 
@@ -19,19 +19,19 @@ function InstallZprintFiler()
 
 	if f~=nil then
 		io.close(f)
-	        print("zprint exists " .. zprint_path)
+	        -- debug("zprint exists " .. zprint_path)
 	else
-	        print("zprint does not exists at " .. zprint_path .. " installing")
+	        -- debug("zprint does not exists at " .. zprint_path .. " installing")
 		local command = "cd " .. home_dir .. " && curl -LJO https://github.com/kkinnear/zprint/releases/download/" .. zprint_version .. "/" .. zprint_file .. " && chmod +x " .. zprint_path
 		-- local command = "cd " .. home_dir .. " && echo 'this is test' > nvim.test" 
 
-		print("Command is " .. command)
+		-- debug("Command is " .. command)
 		local status, err = pcall(os.execute, command)
-		if status then
-			print("command ran successfuly")
-		else
-			print("failed to run command " .. err)
-		end
+		-- if status then
+		-- 	debug("command ran successfuly")
+		-- else
+		-- 	debug("failed to run command " .. err)
+		-- end
 	end
 
 end
