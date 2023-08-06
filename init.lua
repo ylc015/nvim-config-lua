af4fad65cf8a6670923bc081182bbf27add0de8a
Peter Chan <peter.chan@owl.co>
2023-08-06 12:49:20 -0400

    add noice

--- init.lua
+++ init.lua
@@ -46,6 +46,8 @@ require('lazy').setup({
 
   'numToStr/Comment.nvim',
 
+  require 'noice.plugins',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -168,8 +170,8 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -507,4 +509,3 @@ cmp.setup {
 }
 
 require('custom.functions.init').setup()
-
