6fd9385a261ba3f98bdb3ea6fdf05b047f0926a1
Peter Chan <peter.chan@owl.co>
2023-08-14 20:12:33 -0400

    add OSC52 that allows copying to system clipboard from remote machine

--- init.lua
+++ init.lua
@@ -62,6 +62,15 @@ require('lazy').setup({
 
   'numToStr/Comment.nvim',
 
+
+  {
+    'ojroques/nvim-osc52',
+    config = function()
+      vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, { expr = true })
+      vim.keymap.set('n', '<leader>cc', '<leader>c_', { remap = true })
+      vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+    end
+  },
   require 'noice.plugins',
 
   require 'iron.plugins',
