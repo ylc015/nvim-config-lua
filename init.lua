3a8f14727cd090e833cd2b9151c59a420046d9ca
Peter Chan <peter.chan@owl.co>
2023-08-14 20:20:44 -0400

    use OSCyank

--- init.lua
+++ init.lua
@@ -64,13 +64,12 @@ require('lazy').setup({
 
 
   {
-    'ojroques/nvim-osc52',
+    'ojroques/vim-oscyank',
     config = function()
-      vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, { expr = true })
-      vim.keymap.set('n', '<leader>cc', '<leader>c_', { remap = true })
-      vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+      vim.keymap.set('v', '<leader>cc', '<Plug>OSCYankVisual', { silent = true })
     end
   },
+
   require 'noice.plugins',
 
   require 'iron.plugins',
