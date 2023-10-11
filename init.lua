7454cc6c5aa00157d1ced077217558750b77fa61
Peter Chan <peter.chan@owl.co>
2023-10-11 22:45:12 +0000

    add bb command

--- init.lua
+++ init.lua
@@ -376,6 +376,9 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+-- bb related
+vim.keymap.set('n', '<leader>sn', ":vs term://bb nrepl:start --with-test-deps -e dev-owl", { desc = '[S]tart [N]repl' })
+
 -- LuaPad: Evaluaing
 vim.keymap.set('n', '<leader>li', require('luapad').init, { desc = '[L]ua [P]ad' })
 vim.keymap.set('n', '<leader>lr', ":LuaRun<CR>", { desc = '[L]ua [R]un' })
