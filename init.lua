ab68a2c8f180f901556d3b37d40d69761b85a364
Peter Chan <peter.chan@owl.co>
2023-11-20 14:24:01 -0500

    add neotest

--- init.lua
+++ init.lua
@@ -276,6 +276,15 @@ require('lazy').setup({
     opts = {} -- this is equalent to setup({}) function
   },
 
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-python",
+    }
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -665,6 +674,37 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = { justMyCode = false },
+      -- Command line arguments for runner
+      -- Can also be a function to return dynamic values
+      args = { "--log-level", "DEBUG" },
+      -- Runner to use. Will use pytest if available by default.
+      -- Can be a function to return dynamic value.
+      runner = "pytest",
+      -- Custom python path for the runner.
+      -- Can be a string or a list of strings.
+      -- Can also be a function to return dynamic value.
+      -- If not provided, the path will be inferred by checking for
+      -- virtual envs in the local directory and for Pipenev/Poetry configs
+      -- python = ".venv/bin/python",
+      -- Returns if a given file path is a test file.
+      -- NB: This function is called a lot so don't perform any heavy tasks within it.
+      -- is_test_file = function(file_path)
+      --   ...
+      -- end,
+      -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
+      -- instances for files containing a parametrize mark (default: false)
+      pytest_discover_instances = true,
+    })
+  }
+})
+
 
 require('nvim-dap.settings').setup()
 
