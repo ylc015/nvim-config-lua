cd11b11fa83a8cc3c3a4db0e970fb1ab51bd166e
Peter Chan <peter.chan@owl.co>
2023-12-02 17:47:19 -0500

    source vscode

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
+local config_path = vim.fn.stdpath('config')
 if vim.g.vscode then
   -- VSCode extension
   print('using VScode ext', 0)
+  local vscode_lua_config_path = config_path .. '/init-vscode.lua'
+  dofile(vscode_lua_config_path)
 else
   -- ordinary Neovim
   print('using ordinary Neovim', 0)
-  local config_path = vim.fn.stdpath('config')
   local nvim_lua_config_path = config_path .. '/init-nvim.lua'
   dofile(nvim_lua_config_path)
 end
